5d696edc42c0cf7eed638032cbb1fb8d0bc8b9d5
KalsonSaint <saintkalson@gmail.com>
2021-12-29 11:17:33 +0100

    config server

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Outlet } from "remix";
 
 const Home = () => {
   return (
     <div>
-      <h1>Welcome</h1>
+     <h1>Welcome to Remix!</h1>
+      <p>
+        Remix is a full stack web framework by the creators of React Router.
+        This is a simple blog app from the Traversy Media Remix crash course.
+      </p>
     </div>
   );
 };
